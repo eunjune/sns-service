8c73668f8a325a629d1ef3dc9586641ef3cc8270
eunjune <iws1605@naver.com>
2020-03-12 20:59:31 +0900

    react-helmet 추가

--- social-server-ui/pages/_app.js
+++ social-server-ui/pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import withRedux from 'next-redux-wrapper';
 import withReduxSaga from 'next-redux-saga';
@@ -12,20 +13,38 @@ import AppLayout from '../components/AppLayout';
 import rootSaga from '../saga';
 import cookie from 'react-cookies';
 import { LOAD_ME_REQUEST } from '../reducers/user';
-import { Col } from 'antd';
+import {Container} from 'next/app';
 
 const Root = ({ Component, store, pageProps }) => (
-  <Provider store={store}>
-    <Head>
-      <title>SNS</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.26.11/antd.css" />
-      <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-    </Head>
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
-  </Provider>
+  <Container>
+    <Provider store={store}>
+        <Helmet 
+          title="SNS"
+          htmlAttributes={{lang: 'ko'}}
+          meta = {[{
+            charset: 'UTF-8',
+          } , {
+            name: 'viewport', content='width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover'
+          } , {
+            'http-equiv' : 'X-UA-Compatible', content: 'IE-edge',
+          }, {
+            name: 'description', content: 'SNS'
+          }, {
+            property: 'og:type', content: 'website',
+          }]}
+          link={[{
+            rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css',
+          }, {
+            rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css',
+          }, {
+            rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css',
+          }]}
+        />
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </Provider>
+  </Container>
 );
 
 Root.propTypes = {
---
+++ social-server-ui/pages/_document.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Document, { Main, NextScript } from 'next/document';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+
+class MyDocument extends Document {
+    static getInitialProps(context) {
+        const page = context.renderPage((App)=>(props)=><App {...props}/>);
+        return { ...page, helmet: Helmet.renderStatic()} 
+    }
+
+    render() {
+        const { htmlAttributes, bodyAttributes, ...helmet} = this.props.helmet;
+        const htmlAttrs = htmlAttributes.toComponent();
+        const bodyAttrs = bodyAttributes.toComponent();
+        return (
+            <html {...htmlAttrs}>
+                <head>
+                    {Object.values(helmet).map(el => el.toComponent())}
+                </head>
+                <body {...bodyAttrs}>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        )
+    }
+}
+
+MyDocument.propTypes = {
+    helmet: PropTypes.object.isRequired,
+}
+
+export default MyDocument;
